e2070d85296f3e12d2d86def3dd9fb8b463970f1
0xflotus <0xflotus@gmail.com>
2026-03-14 23:17:39 +0100

    Update example5.fixed.js

--- fixture/example5.fixed.js
+++ fixture/example5.fixed.js
@@ -2,8 +2,8 @@ const obj = {
   small: 0x0a,
   medium: 0xff,
   large: 291,
-  bigIntSmall: 254n,
+  bigIntSmall: 0xfen,
   bigIntLarge: 291n,
 };
 
-const arr = [0x5, 0x10, 291, 171n];
+const arr = [0x5, 0x10, 291, 0xabn];
