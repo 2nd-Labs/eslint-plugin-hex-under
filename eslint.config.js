1f2fb0801a924dc1178d67caf8a84688cb7dbc98
0xflotus <0xflotus@gmail.com>
2026-03-17 16:34:45 +0100

    feat: add recommended eslint rules to linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,20 @@
 "use strict";
 
+import js from "@eslint/js";
 import plugin from "./src/eslint-plugin-hex-under.js";
 
 export default [
+  {
+    ...js.configs.recommended,
+    ignores: ["fixture/*.js", "tmp/*.js"],
+  },
   {
     plugins: {
       "hex-under": plugin,
     },
     rules: {
       "hex-under/hex-under": ["error", { limit: 255 }],
-      "hex-under/hex-under-bigint": ["warn"],
+      "hex-under/hex-under-bigint": ["error"],
     },
   },
 ];
