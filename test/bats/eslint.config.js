4b04183b4a4deb9a4f3cf162650d1f5535543d80
0xflotus <0xflotus@gmail.com>
2026-03-24 00:09:08 +0100

    feat: add presets recommended + all

--- test/bats/eslint.config.js
+++ test/bats/eslint.config.js
@@ -1,34 +1,12 @@
 'use strict';
 
-import plugin from '../../src/eslint-plugin-hex-under.js';
+import plugin, { configs } from '../../src/eslint-plugin-hex-under.js';
 
 export default [
   {
     plugins: {
       'hex-under': plugin,
     },
-    rules: {
-      'hex-under/hex-under': [
-        'error',
-        {
-          limit: 255,
-          skipBigInt: false,
-        },
-      ],
-      'hex-under/octal-under': [
-        'error',
-        {
-          limit: 511,
-          skipBigInt: false,
-        },
-      ],
-      'hex-under/binary-under': [
-        'error',
-        {
-          limit: 15,
-          skipBigInt: false,
-        },
-      ],
-    },
+    ...configs.all,
   },
 ];
