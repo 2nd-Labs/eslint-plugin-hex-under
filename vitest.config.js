e4eb8e1b8f79fc18540a620067a9a83de39a5f30
0xflotus <0xflotus@gmail.com>
2026-09-04 16:57:06 +0200

    test: split execution of unit and integration tests (#206)

--- vitest.config.js
+++ vitest.config.js
@@ -2,25 +2,53 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'node',
-    reporters: 'verbose',
-    threads: false,
-    coverage: {
-      provider: 'istanbul',
-      exclude: ['test/integration/**/*.int.test.js'],
-      cleanOnRerun: true,
-      reporter: ['text', 'html', 'json', 'lcov', 'json-summary'],
-      reportsDirectory: './coverage',
-      reportOnFailure: true,
-      skipFull: false,
-      all: true,
-      thresholds: {
-        statements: 80,
-        branches: 80,
-        functions: 80,
-        lines: 80,
+    projects: [
+      {
+        test: {
+          name: 'unit',
+          include: ['test/**/*.test.js'],
+          exclude: ['test/integration/**/*.int.test.js'],
+          sequence: {
+            groupOrder: 0,
+          },
+          environment: 'node',
+          reporters: 'verbose',
+          threads: false,
+          coverage: {
+            provider: 'istanbul',
+            exclude: ['test/integration/**/*.int.test.js'],
+            cleanOnRerun: true,
+            reporter: ['text', 'html', 'json', 'lcov', 'json-summary'],
+            reportsDirectory: './coverage',
+            reportOnFailure: true,
+            skipFull: false,
+            all: true,
+            thresholds: {
+              statements: 80,
+              branches: 80,
+              functions: 80,
+              lines: 80,
+            },
+          },
+          globals: false,
+        },
       },
-    },
-    globals: false,
+      {
+        test: {
+          name: 'integration',
+          include: ['test/integration/**/*.int.test.js'],
+          sequence: {
+            groupOrder: 1,
+          },
+          environment: 'node',
+          coverage: {
+            enabled: false,
+            provider: 'istanbul',
+          },
+          threads: false,
+          globals: false,
+        },
+      },
+    ],
   },
 });
